77489298419784cd29fea0335850456240ae4164
TannerStevens <tanner.stevens42@gmail.com>
2016-04-21 11:23:56 -0400

    Expanded & Organized JUnit Tests

--- src/Graph.java
+++ src/Graph.java
@@ -1,4 +1,3 @@
-import java.awt.List;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
--- src/ParserTest.java
+++ src/ParserTest.java
@@ -1,18 +1,39 @@
 import static org.junit.Assert.*;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class ParserTest {
-
+	Parser p = new Parser();
+	
 	@Test
-	public void test() {
-		Parser p = new Parser();
+	public void testSingleDigits() {
 		assertEquals("2 2 +", p.translate("2+2"));
-		assertEquals("10 2 -", p.translate("10-2"));
+	}
+	
+	@Test
+	public void testMultidigit(){
+		assertEquals("24 200 -", p.translate("24-200"));
+	}
+	
+	@Test
+	public void testCharacterVariables(){
+		assertEquals("x 2 +", p.translate("x+2"));
+		assertEquals("2 x +", p.translate("2+x"));
+	}
+	
+	@Test
+	public void testStringVariables(){
+		assertEquals("var 2 +", p.translate("var+2"));
+		assertEquals("xy 2 +", p.translate("xy+2"));
+		
+		assertEquals("2 var +", p.translate("2+var"));
+		assertEquals("2 xy +", p.translate("2+xy"));
+	}
+	
+	@Test
+	public void testParenthesis(){
 		assertEquals("100 25 2 * -", p.translate("100-(25*2)"));
 		assertEquals("4 2 1 + * 6 10 / -", p.translate("4*(2+1)-6/10"));
-		assertEquals("2 5 2 ^ * 2 - 4 +", p.translate("2*5^2-2+4"));
 	}
 
 }
